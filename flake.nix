af42dc01985a465779ac4b42d49b007eb908ea35
Johannes Steinmetzer <Johannes.Steinmetzer@eah-jena.de>
2026-05-13 09:55:37 +0200

    add helper to push image to registry

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+      # The actual service
       simple-service = pkgs.pkgsStatic.buildGoModule {
         name = "simple-service";
         version = "0.1.0";
@@ -17,23 +18,48 @@
         # There are only stdlib deps
         vendorHash = null;
       };
+      # The corresponding docker image
+      dockerImage = pkgs.dockerTools.buildLayeredImage {
+        name = "simple-service";
+        tag = "latest";
+        
+        contents = [
+          simple-service
+        ];
+        
+        config = {
+          Cmd = [
+            "${simple-service}/bin/simple-service"
+          ];
+        };
+      };
     in
     {
       packages.${system} = {
         default = simple-service;
-        docker-image = pkgs.dockerTools.buildLayeredImage {
-          name = "simple-service";
-          tag = "latest";
-          
-          contents = [
-            simple-service
-          ];
-          
-          config = {
-            Cmd = [
-              "${simple-service}/bin/simple-service"
+        docker-image = dockerImage;
+      };
+      
+      apps.${system} = {
+        push-docker-image = {
+          type = "app";
+          program = "${pkgs.writeShellApplication {
+            name = "push-docker-image";
+            runtimeInputs = [
+              pkgs.skopeo
             ];
-          };
+            text = ''
+              GIT_TAG=$(git describe --tags --always 2>/dev/null)
+              
+              TAGGED_IMAGE="simple-service:$GIT_TAG"
+              echo "Pushing docker image '$TAGGED_IMAGE' to registry '$REGISTRY'"
+
+              skopeo copy docker-archive:${dockerImage} docker://"''${REGISTRY}"/simple-service:"$GIT_TAG" \
+                --policy <(echo '{"default":[{"type":"insecureAcceptAnything"}]}') \
+                --dest-tls-verify=false \
+                --dest-creds="''${REGISTRY_USER}:''${REGISTRY_PASSWORD}"
+            '';
+          }}/bin/push-docker-image";
         };
       };
 
